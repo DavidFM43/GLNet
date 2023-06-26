00fd99f8436c994852e60a6b92907a815e02fb56
David <dmorara@unal.edu.co>
2023-06-25 20:02:14 -0500

    Add data and data script

---
+++ make_data.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+from pathlib import Path
+from tqdm import tqdm
+
+
+def move_data(ids, split, images_dir, masks_dir):
+    """Move data to train/val/test folders."""
+
+    # create folders
+    os.makedirs(data_dir / split, exist_ok=True)
+    split_images_dir = data_dir / split / "Sat"
+    split_masks_dir = data_dir / split / "Label"
+    os.makedirs(split_images_dir, exist_ok=True)
+    os.makedirs(split_masks_dir, exist_ok=True)
+
+    for id in tqdm(ids, desc=f"Preparing {split} data"):
+        old_sat = images_dir / f"{id}_sat.jpg"
+        old_label = masks_dir / f"{id}_mask.png"
+        new_sat = split_images_dir / f"{id}_sat.jpg"
+        new_label = split_masks_dir / f"{id}_mask.png"
+        shutil.copy(old_label, new_label)
+        shutil.copy(old_sat, new_sat)
+
+
+if __name__ == "__main__":
+    data_dir = Path("data")
+    raw_data_dir = data_dir / "raw_data"
+
+    images_dir = raw_data_dir / "images"
+    masks_dir = raw_data_dir / "raw_masks"  # masks are in rgb format
+
+    # read ids
+    with open("train.txt") as f:
+        train_ids = [x.split("_")[0] for x in f.read().splitlines()]
+    with open("crossvali.txt") as f:
+        val_ids = [x.split("_")[0] for x in f.read().splitlines()]
+    with open("test.txt") as f:
+        test_ids = [x.split("_")[0] for x in f.read().splitlines()]
+
+    # move data
+    move_data(train_ids, "train", images_dir=images_dir, masks_dir=masks_dir)
+    move_data(val_ids, "val", images_dir=images_dir, masks_dir=masks_dir)
+    move_data(test_ids, "test", images_dir=images_dir, masks_dir=masks_dir)
